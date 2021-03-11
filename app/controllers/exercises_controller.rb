5a7f4f204e5cc22eb57dcfbca50cd3730a1bcf3e
Phong Lo <phonglo724@gmail.com>
2021-03-11 10:27:18 -0700

    pushing up so I can pull

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,11 +1,11 @@
 class ExercisesController < ApplicationController
     def index
         @exercises = Exercise.all
-        render json: @exercises
+        render json: @exercises, include: :workout
     end
 
     def show
         @exercise = Exercise.find(params[:id])
-        render json: @exercise
+        render json: @exercise, include: :workout
     end
 end
